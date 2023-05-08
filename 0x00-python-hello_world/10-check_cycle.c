9372e46840b5c5e0204edac3feef801e3cb4b4b1
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-05-08 14:52:04 +0100

    interview question

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - check if there is a cycle in a singly  linked list
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	int i = 0;
+	listint_t **past;
+
+	if (!list)
+		return (0);
+	past = malloc(sizeof(listint_t *));
+	past[0] = list;
+	i++;
+	while (list->next != NULL)
+	{
+		list = list->next;
+		int j = 0;
+
+		for (j = 0; j < i; j++)
+		{
+			if (past[j] == list)
+			{
+				free(past);
+				return (1);
+			}
+		}
+		past = realloc(past, sizeof(listint_t *) * (1 + i));
+		past[i] = list;
+		i++;
+	}
+	free(past);
+	return (0);
+}
