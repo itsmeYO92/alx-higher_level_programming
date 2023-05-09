3cefa194d3736e85dad65cc5850e414072c2672f
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-05-09 23:57:27 +0100

    dis

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-import dis
 def magic_calculation(a, b, c):
-    if a < b:
-        if c > b:
+    if a > b:
+        if c < b:
             return ((a * b) - c)
         else:
             return (a + b)
     else:
         return (c)
 
-dis.dis(magic_calculation)
--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
+#!/usr/bin/python3
+magic_calculation = __import__('102-magic_calculation')
+print(magic_calculation.magic_calculation(1, 2, 3))
 
-fizzbuzz()
-print("")
