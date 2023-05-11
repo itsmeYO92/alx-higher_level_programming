8657a8f2fa4cd3f06a6f42546ac24a876fcaab2b
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-05-11 14:40:45 +0100

    easy print

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,11 +1,2 @@
 #!/usr/bin/python3
-import os
-
-
-def main():
-    a = "#pythoniscool\n"
-    os.write(1, a.encode())
-
-
-if __name__ == "__main__":
-    main()
+__import__("os").write(1, b"#pythoniscool\n")
