eeb531a93dbacd7028cf2bf1ef95193ba6816720
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-05-11 13:41:04 +0100

    calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import calculator_1 as calc
+
+
+def main():
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
+
+
+if __name__ == "__main__":
+    main()
