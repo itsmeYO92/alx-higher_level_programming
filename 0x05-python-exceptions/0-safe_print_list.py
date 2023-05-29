c3a21c718d746abfaa18788243a459911bf1eeda
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-05-29 19:19:21 +0100

    safeprint

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    nb_of_elements = 0
+    for i, element in enumerate(my_list):
+        if i < x:
+            try:
+                print("{}".format(element), end="")
+                nb_of_elements += 1
+            except BaseException:
+                pass
+    print("")
+    return (nb_of_elements)
