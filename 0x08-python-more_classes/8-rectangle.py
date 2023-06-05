dd3b90dadb3737b24fb172d83f352fa9d5cecb1c
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-05 19:17:18 +0100

    doc

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -80,6 +80,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """ compare two rectangles"""
         if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
