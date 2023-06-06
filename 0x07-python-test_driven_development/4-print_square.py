fec113ae3f5c957716329ea676a316f0089eff1f
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-07 00:52:50 +0100

    change char

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    divide matrix module
+"""
+
+
+def print_square(size):
+    """ say_my_name: prints the full name"""
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    divide matrix module
+"""
+
+def text_indentation(text):
+    """
+       new line after evry ? . or :
+       args:
+           text (str): text to change
+       return:
+           None
+     """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    new_text = ""
+    changed = False
+
+    for i in text:
+        if ":?.".find(i) != -1:
+            new_text += "\n\n"
+            changed = True
+        elif i == " " and changed:
+            continue
+        else:
+            new_text += i
+            changed = False
+    print(new_text)        
