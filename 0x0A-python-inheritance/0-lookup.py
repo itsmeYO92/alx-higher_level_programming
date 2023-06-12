71865d028b00ed3f6c242646b3e4f8f287a4af1c
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-12 18:15:44 +0100

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" lookup module to list all methods and attributes of an object"""
+
+
+def lookup(object):
+    """ list the attributes and methos of an object"""
+    return dir(object)
