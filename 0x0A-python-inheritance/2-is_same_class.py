117f1a44856488e9bf98c7a31fdb48ed46b91a2f
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-12 19:25:26 +0100

    is same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" is_same_class function to compare two classes"""
+
+
+def is_same_class(obj, a_class):
+    """ list the attributes and methos of an object"""
+    return obj.__class__ is a_class
