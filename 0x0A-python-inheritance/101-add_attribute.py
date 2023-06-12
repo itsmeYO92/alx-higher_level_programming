f6d3782a20af3a2ed77937830d6f370d094be9df
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-12 23:58:55 +0100

    add attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Square module defines a square by its size """
+
+
+def add_attribute(obj, attribute, value):
+    """ class MyInt inherints from int"""
+    if hasattr(obj, "__dict__"):
+        setattr(obj, attribute, value)
+    else:
+        raise TypeError("can't add new attribute") 
