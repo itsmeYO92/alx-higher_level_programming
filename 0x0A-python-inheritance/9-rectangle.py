00403195d967fb0fc16707fdcd83de531dedd549
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-12 20:34:43 +0100

    full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -8,13 +8,7 @@ BG = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BG):
     """ list the attributes and methos of an object"""
     def area(self):
-        return self.__width * self.__height 
-
-    def integer_validator(self, name, value):
-        if type(value) not in [int, float]:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        return self.__width * self.__height
 
     def __init__(self, width, height):
         self.integer_validator("width", width)
