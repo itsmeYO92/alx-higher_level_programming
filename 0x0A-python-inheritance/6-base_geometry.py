23e571f687509e7192fed3401393a0315f5426db
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-12 19:54:04 +0100

    area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" is_same_class function to compare two classes"""
+
+
+class BaseGeometry():
+    """ list the attributes and methos of an object"""
+    def area(self):
+        raise Exception("area() is not implemented")
