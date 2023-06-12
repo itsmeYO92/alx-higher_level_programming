4c378906238b1b68d735c92554175acc2ad8869c
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-12 19:31:07 +0100

    same class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" is_same_class function to compare two classes"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ list the attributes and methos of an object"""
+    return isinstance(obj, a_class)
