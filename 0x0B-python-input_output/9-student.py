e3fa9d70f6c6886c62dcefbf8f77f46b134f303f
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-13 19:35:56 +0100

    student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Student module """
+
+
+class Student():
+    """ a class represents a student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        new_dict = {}
+        for i in dir(self):
+            if not (i.startswith("__") or callable(getattr(self, i))):
+                new_dict[i] = getattr(self, i)
+
+        return new_dict
--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+Student = __import__('9-student').Student
 
-filename = "my_list.json"
-my_list = [1, 2, 3]
-save_to_json_file(my_list, filename)
+students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
 
-filename = "my_dict.json"
-my_dict = { 
-    'id': 12,
-    'name': "John",
-    'places': [ "San Francisco", "Tokyo" ],
-    'is_active': True,
-    'info': {
-        'age': 36,
-        'average': 3.14
-    }
-}
-save_to_json_file(my_dict, filename)
-
-try:
-    filename = "my_set.json"
-    my_set = { 132, 3 }
-    save_to_json_file(my_set, filename)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+for student in students:
+    j_student = student.to_json()
+    print(type(j_student))
+    print(j_student['first_name'])
+    print(type(j_student['first_name']))
+    print(j_student['age'])
+    print(type(j_student['age']))
