b5a226e9bd0bc079f50e180495ffa9c26d73c162
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-19 13:18:28 +0100

    from json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,5 +20,29 @@ class Base():
         """return JSON representation of an object"""
         if my_obj is None:
             return("[]")
-        return '[' + json.dumps(my_obj) + ']'
+        return json.dumps(my_obj)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ a class method that saves the json string to a file """
+        file_name = cls.__name__ + ".json"
+        list_dict = []
+
+        if list_objs:
+            for obj in list_objs:
+                if isinstance(obj, Base):
+                    list_dict.append(obj.to_dictionary())
+
+        json_string = Base.to_json_string(list_dict)
+        with open(file_name, "w") as f:
+            f.write(json_string)
+        f.close
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns a list of json string """
+
+        if json_string is None:
+            return []
+        return json.loads(json_string)
 
