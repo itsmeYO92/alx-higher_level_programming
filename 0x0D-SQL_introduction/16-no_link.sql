8365bfdb9eafaf8ec2d31e3de6de404e99862734
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-07-18 11:22:50 +0100

    last one

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,3 @@
 -- lists all records of the table
-SELECT score, name FROM scond_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM scond_table WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
