d6f2ce064f70b4ed7151fcabdaf05fc2a508254c
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-07-18 14:13:33 +0100

    convert to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- chage database to utf8
+ALTER DATABASE hbtn_0c_0 CHARACHTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT CHARACHTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
