161d1423fc411cb005a9d7577bf68460f6636775
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-07-19 17:08:34 +0100

    adding database

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,5 +4,5 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT,
                      state_id INT NOT NULL,
                      name VARCHAR(265) NOT NULL;
-                     FOREIGN KEY (state_id) REFERENCES states(id)
+                     FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
                      PRIMARY KEY (id));
