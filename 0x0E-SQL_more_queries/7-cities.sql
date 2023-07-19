385266d3f3eed53f2c1e00184db3f2bc9ace3448
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-07-19 17:07:01 +0100

    primary

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,4 +4,5 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT,
                      state_id INT NOT NULL,
                      name VARCHAR(265) NOT NULL;
-                     FOREIGN KEY (state_id) REFERENCES states(id));
+                     FOREIGN KEY (state_id) REFERENCES states(id)
+                     PRIMARY KEY (id));
