789b810a7f58744c6f380ad484f5edcc3fcc4214
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-07-19 17:10:13 +0100

    syntax error

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,7 @@
 -- create database hbtn_0d_usa and table "cities"
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT,
                      state_id INT NOT NULL,
-                     name VARCHAR(265) NOT NULL;
-                     FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+                     name VARCHAR(265) NOT NULL,
+                     FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
                      PRIMARY KEY (id));
