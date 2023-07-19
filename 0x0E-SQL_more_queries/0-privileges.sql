f5c0c4871ef4781a82c0fff99511dd6737fe8a21
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-07-19 10:45:42 +0100

    create user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- list the previleges granted to a user
-SHOW GRANTS FOR 'user_0d_1'@'localhost' and 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
