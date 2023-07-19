6d123dca9052e3ae1db9755ef3a63def80f31c3e
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-07-19 09:54:20 +0100

    typos

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- chage database to utf8
-ALTER DATABASE hbtn_0c_0 CHARACHTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-ALTER TABLE first_table CONVERT CHARACHTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
