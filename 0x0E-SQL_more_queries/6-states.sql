c55daaf9f8179d2c642a0537deb904b61198d8f4
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-07-19 16:54:12 +0100

    states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- create a data base hbtn_0d_usa and a table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT,
+                                    name VARCHAR(265) NOT NULL,
+                                    PRIMARY KEY (id));
