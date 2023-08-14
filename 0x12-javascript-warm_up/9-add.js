44b0f2152760971fe8e9e888828acb946837050b
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-08-14 12:11:02 +0100

    add function

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function add(a, b) {
+  if (!parseInt(a) || !parseInt(b)) {
+    return NaN;
+  }
+
+  return parseInt(a) + parseInt(b);
+}
+
+const a = process.argv[2];
+const b = process.argv[3];
+
+console.log(add(a, b));
