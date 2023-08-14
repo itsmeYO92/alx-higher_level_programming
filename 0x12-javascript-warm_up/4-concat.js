876c28efcd7bc8ad12319241c776525c8c35654b
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-08-14 11:03:33 +0100

    code refactoring: undefined is the default value of a variable

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 const arg_array = process.argv;
 
-let myVar = arg_array[2] ? arg_array[2] : "undefined";
-myVar += " is " + (arg_array[3] ? arg_array[3] : "undefined");
-console.log(myVar)
+console.log(`${arg_array[2]} is ${arg_array[3]}`);
