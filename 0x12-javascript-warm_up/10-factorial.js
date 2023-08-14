2cf13ef9870d2efd96984840c7954041d2bd6fad
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-08-14 12:17:56 +0100

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function factorial(a) {
+  if (!a || a === 0) {
+    return 1;
+  }
+
+  return a * factorial(a - 1);
+}
+
+const a = process.argv[2];
+
+console.log(factorial(parseInt(a)));
