1e5746b22caac99d080bc12d3145ca7ec243c1ef
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-08-14 11:55:29 +0100

    x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const x = process.argv[2];
+
+let i = parseInt(x);
+if (!i) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let j = 0; j < i; j++) {
+    console.log("C is fun");
+  }
+}
