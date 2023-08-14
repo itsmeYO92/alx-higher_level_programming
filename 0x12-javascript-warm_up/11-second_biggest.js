252674a6b92d8f923730e5543f2ed719fbed0f1f
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-08-14 12:32:58 +0100

    handling the negative numbers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ const arg_array = process.argv;
 if (arg_array.length < 4) {
   console.log(0);
 } else {
-  let sorted_array = arg_array.slice(2).sort();
+  let sorted_array = arg_array.map(Number).slice(2).sort((a, b) => a - b);
   console.log(sorted_array[sorted_array.length - 2])
 }
