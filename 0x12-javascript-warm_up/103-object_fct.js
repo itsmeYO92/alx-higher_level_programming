1eba38d45ae744f7b82fe26d277659d93fc0706e
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-08-16 18:24:33 +0100

    increment object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,13 +4,14 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-/*
-YOUR CODE HERE
-*/
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
 
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
