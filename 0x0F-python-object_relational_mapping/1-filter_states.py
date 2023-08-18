0eee015ab2c73a1fd0aab8268ac782622ab57f5a
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-08-18 11:57:45 +0100

    filter name

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    select state module that select all stats from a database
+"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    """
+        selects the states
+    """
+    db_args = sys.argv
+    db = MySQLdb.connect(
+        "localhost", db_args[1], db_args[2], db_args[3], port=3306)
+
+    db_cursor = db.cursor()
+    db_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+
+    results = db_cursor.fetchall()
+    for row in results:
+        print(row)
+
+    db_cursor.close()
+    db.close()
