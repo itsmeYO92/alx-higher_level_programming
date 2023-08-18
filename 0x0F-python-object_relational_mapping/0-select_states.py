4d905771930afabf7e2d6ba40b693c8a7954ee1e
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-08-18 11:54:57 +0100

    code refactoring and pycodestyling

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@
 """
 
 
-from MySQLdb import _mysql
+import MySQLdb
 import sys
 
 
@@ -13,14 +13,15 @@ if __name__ == '__main__':
         selects the states
     """
     db_args = sys.argv
-    db = _mysql.connect("localhost", db_args[1], db_args[2], db_args[3], port=3306)
-    
-    db.query("""SELECT * FROM states;""")
-    
-    results = db.store_result()
-    rows = results.fetch_row(maxrows=0)
-    decoded_rows = [(row[0].decode(), row[1].decode()) for row in rows]
-    for row in decoded_rows:
+    db = MySQLdb.connect(
+        "localhost", db_args[1], db_args[2], db_args[3], port=3306)
+
+    db_cursor = db.cursor()
+    db_cursor.execute("SELECT * FROM states")
+
+    results = db_cursor.fetchall()
+    for row in results:
         print(row)
-    
+
+    db_cursor.close()
     db.close()
