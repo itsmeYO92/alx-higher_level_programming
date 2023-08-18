52f06071877d436a886ab0434d9df4f951660311
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-08-18 11:47:02 +0100

    adding the port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,15 +8,19 @@ from MySQLdb import _mysql
 import sys
 
 
-db_args = sys.argv
-db = _mysql.connect("localhost", db_args[1], db_args[2], db_args[3])
-
-db.query("""SELECT * FROM states;""")
-
-results = db.store_result()
-rows = results.fetch_row(maxrows=0)
-decoded_rows = [(row[0].decode(), row[1].decode()) for row in rows]
-for row in decoded_rows:
-    print(row)
-
-db.close()
+if __name__ == '__main__':
+    """
+        selects the states
+    """
+    db_args = sys.argv
+    db = _mysql.connect("localhost", db_args[1], db_args[2], db_args[3], port=3306)
+    
+    db.query("""SELECT * FROM states;""")
+    
+    results = db.store_result()
+    rows = results.fetch_row(maxrows=0)
+    decoded_rows = [(row[0].decode(), row[1].decode()) for row in rows]
+    for row in decoded_rows:
+        print(row)
+    
+    db.close()
