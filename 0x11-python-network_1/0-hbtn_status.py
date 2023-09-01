df8cb54f223a48c3212b9ae19fd517588b8fd028
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-09-01 10:57:44 +0100

    if name = main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,13 +4,14 @@
 import urllib.request
 
 
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    my_str = '''Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}'''.format(type(content),
-                                 content,
-                                 content.decode("utf-8"))
-    print(my_str)
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        my_str = '''Body response:
+        - type: {}
+        - content: {}
+        - utf8 content: {}'''.format(type(content),
+                                     content,
+                                     content.decode("utf-8"))
+        print(my_str)
