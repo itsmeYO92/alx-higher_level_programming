669759c43c7deed855c5288081d80e005d8b1509
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-09-05 14:57:49 +0100

    post

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" a Python script that fetches a url"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({"email": email}).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
