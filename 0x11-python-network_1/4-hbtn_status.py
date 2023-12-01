672d46587747f19629330a0fbad8652cfd407b05
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-12-01 15:17:30 +0100

    decoding content

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" a Python script that fetches a url"""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read().decode("utf-8")
+        my_str = '''Body response:
+\t- type: {}
+\t- content: {}'''.format(type(content),
+                               content)
+        print(my_str)
