f4150561ae78eb75eb57f5613628d7a16737a97a
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-12-01 15:28:10 +0100

    without with

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,9 +6,9 @@ import urllib.request
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        content = response.read().decode("utf-8")
-        my_str = '''Body response:
+    response = urllib.request.urlopen(url)
+    content = response.read().decode("utf-8")
+    my_str = '''Body response:
 \t- type: {}
 \t- content: {}'''.format(type(content), content)
-        print(my_str)
+    print(my_str)
