e1c1517b455d7d4f82818ef3558e39c815e419f7
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-12-01 16:29:51 +0100

    json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a Python script that fetches a url"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = "" if len(sys.argv) < 2 else sys.argv[1]
+    payload = {'q': q}
+    r = requests.post(url, data=payload).json()
+    if r == {}:
+        print("No result")
+    try:
+        print("[{}]: {}".format(r.get("id"), r.get("name")))
+    except ValueError:
+        print("Not a valide JSON")
