c01cd33dee38fc4eade1da7421ae20015f5f6752
itsmeYO92 <ay.aboutarbouch@gmail.com>
2024-01-03 18:57:06 +0100

    counting tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const endPoint = process.argv[2];
+
+request.get(endPoint, {json: true }, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  let count = {};
+  body.forEach((task) => {
+    if (task.completed) {
+      if (!count[task.userId]) {
+        count[task.userId] = 1;
+      } else {
+        count[task.userId] += 1;
+      }
+    };
+  });
+  console.log(count);
+});
